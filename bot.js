3198b1e7d597d3d66685eb290aaae4f49ad00b1e
Steven Rollet <57866787+stevenray3@users.noreply.github.com>
2023-01-03 11:51:34 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+// const { trigger_words } = require(`./trigger-words.json`);
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    msg.send("Bonjour !");
+});
+
+client.on('message', message => {
+    if (message.content === '!tavernier salut') {
+        // Get the user's nickname
+        let nickname = message.member.nickname;
+        if (nickname === null) {
+            // If the user doesn't have a nickname, use their username
+            nickname = message.author.username;
+        }
+        // Send the greeting message with the user's nickname
+        message.channel.send(`Salut, ${nickname}! Bienvenue à la Taverne!`);
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
